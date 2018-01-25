ea006ab6e77a3f605d8f73a577067f21ff28a2db
Jack Defay <3jackattack3@gmail.com>
2018-01-24 23:18:53 -0500

    added code for writing to motors via motor controller board - untested

--- roverReceiver/src/main.cpp
+++ roverReceiver/src/main.cpp
@@ -2,6 +2,8 @@
 #include <SPI.h>
 #include <RH_RF69.h>
 
+#define JOYSTICK_RANGE 1020
+
 #define PIN 5
 
 #define RF69_FREQ 900.0
@@ -12,6 +14,23 @@
   #define LED           13
 
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
+
+#define rfpos 2
+#define rfneg 4
+#define rbpos 7
+#define rbneg 8
+#define lfpos 10
+#define lfneg 11
+#define lbpos 12
+#define lbneg 13
+
+int pwmrf = 3;
+int pwmrb = 5;
+int pwmlf = 6;
+int pwmlb = 9;
+
+void setDirection(char motor, int direction);
+void setSpeed(int pwmr, int pwml);
 
 void setup() {
   Serial.begin(115200);
@@ -41,23 +60,38 @@ void setup() {
   rf69.setEncryptionKey(key);
 
   pinMode(LED, OUTPUT);
+
+  pinMode(rfpos, OUTPUT);
+  pinMode(rfneg, OUTPUT);
+  pinMode(rbpos, OUTPUT);
+  pinMode(rbneg, OUTPUT);
+  pinMode(lfpos, OUTPUT);
+  pinMode(lfneg, OUTPUT);
+  pinMode(lbpos, OUTPUT);
+  pinMode(lbneg, OUTPUT);
+  pinMode(pwmrf, OUTPUT);
+  pinMode(pwmrb, OUTPUT);
+  pinMode(pwmlf, OUTPUT);
+  pinMode(pwmlb, OUTPUT);
+
+  setDirection('r', 1);
+  setDirection('l', 1);
 }
+
 void loop(){
   String word, xcoord, ycoord;
   char temp[20];
-  int i = 0, xcoordint, ycoordint;
+  int i = 0, xcoordint, ycoordint, pwmr = 0, pwml = 0;
+
+  setSpeed(pwmr pwml);
+
   if (rf69.available()) {
-      // Should be a message for us now
       uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
       uint8_t len = sizeof(buf);
       if (rf69.recv(buf, &len)) {
         if (!len) return;
         buf[len] = 0;
-        //Serial.print("Received [");
-        //Serial.print(len);
-        //Serial.print("]: ");
         Serial.println((char*)buf);
-        //temp = (char) buf;
         for(int i = 0; i<20; i++){
           temp[i] = (char) buf[i];
         }
@@ -71,26 +105,45 @@ void loop(){
           if(word.charAt(i) >= '0' && word.charAt(i) <= '9') ycoord += word.charAt(i);
           i++;
         }
-        /*for(int i = 0; i<5; i++){
-          if(word.charAt(i) >= '0' && word.charAt(i) <= '9') xcoord += word.charAt(i);
-        }
-        for(int i = 5; i<10; i++){
-          if(word.charAt(i) >= '0' && word.charAt(i) <= '9') ycoord += word.charAt(i);
-        }*/
-        //xcoord = word.substring(0, 4);
-        //ycoord = word.substring(8, 13);
         xcoordint = xcoord.toInt();
         ycoordint = ycoord.toInt();
         Serial.print(xcoordint); Serial.print(", "); Serial.println(ycoordint);
-        //Serial.println(word);
-        //Serial.print(", ");
-        //Serial.print("RSSI: ");
-        //Serial.println(rf69.lastRssi(), DEC);
-        //blink('r', (unsigned int) 200);
+
+        //set pwm levels for writing to motors!
+        pwmr = (int) ycoordint * 255;  //***just going to code it to do turns for now, will add rotation functionality later***
+        pwmr /= (int) JOYSTICK_RANGE;
+        pwml = pwmr;
+        if(xcoorint > JOYSTICK_RANGE/2) pwmr -= (int) (xcoordint * 255)/JOYSTICK_RANGE;
+        else if(xcoordint < JOYSTICK_RANGE/2) pwml -= (int) (xcoordint * 255)/JOYSTICK_RANGE;
+
         digitalWrite(13, HIGH);
-      } else {
+      }
+
+      else {
         Serial.println("Receive failed");
       }
    }
    else digitalWrite(13, LOW);
+}
+
+void setDirection(char motor, bool direction){  //1 = forwards, 0 = backwards
+  if(motor == 'r'){
+    digitalWrite(rfpos, (int) direction);
+    digitalWrite(rfneg, (int) !direction);
+    digitalWrite(rbpos, (int) direction);
+    digitalWrite(rbneg, (int) !direction);
+  }
+  else if(motor == 'l'){                        //the left wheels are reversed
+    digitalWrite(lfpos, (int) !direction);
+    digitalWrite(lfneg, (int) direction);
+    digitalWrite(lbpos, (int) !direction);
+    digitalWrite(lbneg, (int) direction);
+  }
+}
+
+void setSpeed(int pwmr, int pwml){
+  analogWrite(pwmrf, pwmr);
+  analogWrite(pwmrb, pwmr);
+  analogWrite(pwmlf, pwml);
+  analogWrite(pwmlb, pwml);
 }
