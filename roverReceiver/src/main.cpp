d86946deb8c74bc5d87efe3c2c585978cba85e8e
3jackattack3 <3jackattack3@gmail.com>
2018-02-15 20:46:27 -0500

    idk

--- roverReceiver/src/main.cpp
+++ roverReceiver/src/main.cpp
@@ -1,19 +1,19 @@
 #include <Arduino.h>
 #include <SPI.h>
-//#include <RH_RF69.h>
+#include <RH_RF69.h>
 
 #define JOYSTICK_RANGE 1020
 
-//#define PIN 5
+#define PIN 5
 
-/*#define RF69_FREQ 900.0
+#define RF69_FREQ 900.0
 
   #define RFM69_CS      8
   #define RFM69_INT     3
   #define RFM69_RST     4
-  //#define LED           13
+  #define LED           13
 
-RH_RF69 rf69(RFM69_CS, RFM69_INT);*/
+RH_RF69 rf69(RFM69_CS, RFM69_INT);
 
 #define rfpos A5
 #define rfneg A2
@@ -29,16 +29,15 @@ RH_RF69 rf69(RFM69_CS, RFM69_INT);*/
 #define pwmlf A3
 #define pwmlb A4
 
-// void setDirection(char motor, bool direction);
-// void setSpeed(int pwmr, int pwml);
+void setDirection(char motor, bool direction);
+void setSpeed(int pwmr, int pwml);
 
 void setup() {
   Serial.begin(115200);
-  //while(!Serial);
 
   Serial.println("starting");
 
-  /*pinMode(LED, OUTPUT);
+  pinMode(LED, OUTPUT);
   pinMode(RFM69_RST, OUTPUT);
   digitalWrite(RFM69_RST, LOW);
 
@@ -61,7 +60,7 @@ void setup() {
                     0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
   rf69.setEncryptionKey(key);
 
-  pinMode(LED, OUTPUT);*/
+  pinMode(LED, OUTPUT);
 
   pinMode(rfpos, OUTPUT);
   pinMode(rfneg, OUTPUT);
@@ -76,19 +75,19 @@ void setup() {
   pinMode(pwmlf, OUTPUT);
   pinMode(pwmlb, OUTPUT);
 
-  // setDirection('r', 1);
-  // setDirection('l', 1);
+  setDirection('r', 1);
+  setDirection('l', 1);
 
 }
 
 void loop(){
   String word, xcoord, ycoord;
-  //char temp[20];
-  int i = 0, xcoordint, ycoordint;//, pwmr = 0, pwml = 0;
-//  static unsigned long previousMillis = 0, currentMillis = 0;
+  char temp[20];
+  int i = 0, xcoordint, ycoordint, pwmr = 0, pwml = 0;
+ static unsigned long previousMillis = 0, currentMillis = 0;
 static int counter = 0;
 
-/*  if (rf69.available()) {
+  if (rf69.available()) {
       uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
       uint8_t len = sizeof(buf);
       if (rf69.recv(buf, &len)) {
@@ -98,7 +97,7 @@ static int counter = 0;
         for(int i = 0; i<20; i++){
           temp[i] = (char) buf[i];
         }
-        word = temp;*/
+        word = temp;
         i = 0;
         word = "500* 500*";
         while(word.charAt(i) != '*'){
@@ -115,18 +114,18 @@ static int counter = 0;
         Serial.print(xcoordint); Serial.print(", "); Serial.println(ycoordint);
 
         //set pwm levels for writing to motors!
-        // pwmr = (int) ycoordint * 255;  //***just going to code it to do turns for now, will add rotation functionality later***
-        // pwmr /= (int) JOYSTICK_RANGE;
-        // pwml = pwmr;
-        // if(xcoordint > JOYSTICK_RANGE/2) pwmr -= (int) (xcoordint * 255)/JOYSTICK_RANGE;
-        // else if(xcoordint < JOYSTICK_RANGE/2) pwml -= (int) (xcoordint * 255)/JOYSTICK_RANGE;
-        //
-        // setSpeed(pwmr, pwml);
-        // Serial.print(pwmr); Serial.print(", "); Serial.println(pwml);
-        //previousMillis = millis();
+        pwmr = (int) ycoordint * 255;  //***just going to code it to do turns for now, will add rotation functionality later***
+        pwmr /= (int) JOYSTICK_RANGE;
+        pwml = pwmr;
+        if(xcoordint > JOYSTICK_RANGE/2) pwmr -= (int) (xcoordint * 255)/JOYSTICK_RANGE;
+        else if(xcoordint < JOYSTICK_RANGE/2) pwml -= (int) (xcoordint * 255)/JOYSTICK_RANGE;
 
-        //digitalWrite(13, HIGH);
-      /*}
+        setSpeed(pwmr, pwml);
+        Serial.print(pwmr); Serial.print(", "); Serial.println(pwml);
+        previousMillis = millis();
+
+        digitalWrite(13, HIGH);
+      }
 
       else {
         Serial.println("Receive failed");
@@ -135,33 +134,33 @@ static int counter = 0;
    else {
      digitalWrite(13, LOW);
      Serial.println("no signal");
-   }*/
+   }
 
-   //currentMillis = millis();
-   //if(currentMillis - previousMillis > 100) setSpeed(0, 0);
+   currentMillis = millis();
+   if(currentMillis - previousMillis > 100) setSpeed(0, 0);
    delay(100);
    Serial.println(counter);
 }
 
-// void setDirection(char motor, bool direction){  //1 = forwards, 0 = backwards
-//   Serial.println("In function setDirection");
-//   if(motor == 'r'){
-//     digitalWrite(rfpos, (int) direction);
-//     digitalWrite(rfneg, (int) !direction);
-//     digitalWrite(rbpos, (int) direction);
-//     digitalWrite(rbneg, (int) !direction);
-//   }
-//   else if(motor == 'l'){                        //the left wheels are reversed
-//     digitalWrite(lfpos, (int) !direction);
-//     digitalWrite(lfneg, (int) direction);
-//     digitalWrite(lbpos, (int) !direction);
-//     digitalWrite(lbneg, (int) direction);
-//   }
-// }
-//
-// void setSpeed(int pwmr, int pwml){
-//   analogWrite(pwmrf, pwmr);
-//   analogWrite(pwmrb, pwmr);
-//   analogWrite(pwmlf, pwml);
-//   analogWrite(pwmlb, pwml);
-// }
+void setDirection(char motor, bool direction){  //1 = forwards, 0 = backwards
+  Serial.println("In function setDirection");
+  if(motor == 'r'){
+    digitalWrite(rfpos, (int) direction);
+    digitalWrite(rfneg, (int) !direction);
+    digitalWrite(rbpos, (int) direction);
+    digitalWrite(rbneg, (int) !direction);
+  }
+  else if(motor == 'l'){                        //the left wheels are reversed
+    digitalWrite(lfpos, (int) !direction);
+    digitalWrite(lfneg, (int) direction);
+    digitalWrite(lbpos, (int) !direction);
+    digitalWrite(lbneg, (int) direction);
+  }
+}
+
+void setSpeed(int pwmr, int pwml){
+  analogWrite(pwmrf, pwmr);
+  analogWrite(pwmrb, pwmr);
+  analogWrite(pwmlf, pwml);
+  analogWrite(pwmlb, pwml);
+}
